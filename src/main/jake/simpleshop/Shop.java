710135bb23ea0bf688bbf1a62262c3f751b3a958
jake <jlariv95@gmail.com>
2021-03-25 11:27:38 -0400

    additional function to handle infinite shops

--- src/main/jake/simpleshop/Shop.java
+++ src/main/jake/simpleshop/Shop.java
@@ -1,7 +1,13 @@
 package main.jake.simpleshop;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import javax.annotation.Nonnull;
 
 public class Shop {
 
@@ -10,6 +16,8 @@ public class Shop {
     private int amountPerSell;
     private int cost;
     private int buffer;
+    private boolean infinite;
+    private SimpleShop plugin = SimpleShop.getPlugin(SimpleShop.class);
 
     public Shop(ItemStack item, Player seller, int amountPerSell, int cost, int buffer) {
         this.item = item;
@@ -17,6 +25,7 @@ public class Shop {
         this.amountPerSell = amountPerSell;
         this.cost = cost;
         this.buffer = buffer;
+        this.infinite = buffer == -1;
     }
 
     public ItemStack getItem() {
@@ -38,4 +47,38 @@ public class Shop {
     public int getBuffer() {
         return buffer;
     }
+
+    public boolean isInfinite() {
+        return infinite;
+    }
+
+    public void sell(){
+        if(!infinite) {
+            buffer -= amountPerSell;
+            seller.sendMessage("Someone has purchased your " + item.getType().name());
+            int slot = getEmptyOrCompatibleSlot();
+            PlayerInventory playerInv = seller.getInventory();
+            if (slot == -1) {
+                seller.sendMessage(ChatColor.RED + "Your inventory is full. Your payment has been dropped on the ground.");
+                seller.getWorld().dropItem(seller.getLocation(), new ItemStack(plugin.currency, cost));
+            }
+            if (playerInv.getItem(slot) == null) {
+                playerInv.setItem(slot, new ItemStack(plugin.currency, cost));
+            } else {
+                playerInv.addItem(new ItemStack(plugin.currency, cost));
+            }
+        }
+    }
+
+    private int getEmptyOrCompatibleSlot(){
+        PlayerInventory playerInv = seller.getInventory();
+        for(int i = 0; i < playerInv.getSize(); i++){
+            ItemStack slotStack = playerInv.getItem(i);
+            if(slotStack == null || slotStack.getType() == plugin.currency){
+                return i;
+            }
+        }
+        return -1;
+    }
+
 }
