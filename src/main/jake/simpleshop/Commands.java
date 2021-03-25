3fe3d96cdc42a41a1d9614b82c422b07e076d785
jake <jlariv95@gmail.com>
2021-03-25 11:26:34 -0400

    added commands for creating infinite shops and removing shops

--- src/main/jake/simpleshop/Commands.java
+++ src/main/jake/simpleshop/Commands.java
@@ -1,14 +1,24 @@
 package main.jake.simpleshop;
 
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.craftbukkit.libs.jline.internal.Log;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Commands implements CommandExecutor, Listener {
 
     public final String shop = "openshop";
+    public final String adminShop = "adminshop";
+    public final String removeShop = "removeshop";
     private SimpleShop plugin;
 
     public Commands(SimpleShop plugin) {
@@ -25,6 +35,43 @@ public class Commands implements CommandExecutor, Listener {
                     commandSender.sendMessage("You must be a player to use this command");
                 }
                 return true;
+            case adminShop:
+                if(commandSender instanceof Player && commandSender.isOp() || commandSender instanceof Player && commandSender.hasPermission("simpleshop.adminshop")){
+                    plugin.setupAndOpenAddItem((Player) commandSender, true);
+                }else{
+                    commandSender.sendMessage("You must be a player and op to use this command");
+                }
+                return true;
+            case removeShop:
+                if(commandSender instanceof Player){
+                    plugin.setupRemoveShop((Player) commandSender);
+                }else{
+                    commandSender.sendMessage("You must be a player to use this command");
+                }
+                return true;
+
+
+//            case "test":
+//                File testFile = new File("plugins/SimpleShop/test.txt");
+//                File finalFile = new File("plugins/SimpleShop/final.txt");
+//
+//                try{
+//                    Scanner scanner = new Scanner(testFile);
+//                    FileWriter writer = new FileWriter(finalFile);
+//
+//                    while(scanner.hasNextLine()){
+//                        String mat = scanner.nextLine().replace("\t", "").replace(" ", "");
+//                        Material m = Material.getMaterial(mat);
+//                        if(m != null)
+//                            writer.write(m.name() + "\n");
+//                        Log.info(mat + ":" + m);
+//                    }
+//                    writer.close();
+//                }catch (IOException ignored){
+//
+//                }
+//
+//                return true;
         }
         return false;
     }
